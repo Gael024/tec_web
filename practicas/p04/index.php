9a1e96372e7ad122d16d8b160d27dd02bfb0ade2
Erwin <56erwinavendano@gmail.com>
2024-09-11 06:07:58 -0600

    Ejercicio 2

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>Práctica 3</title>
+    <title>Práctica 4</title>
 </head>
 <body>
     <h2>Ejercicio 1</h2>
@@ -33,7 +33,46 @@
     ?>
  
      <h2>Ejercicio 2</h2>
-     <p>Proporcionar los valores de $a, $b, $c como sigue:</p> 
+     <p>Proporcionar los valores de $a, $b, $c como se indica:</p> 
+     <?php
+     
+     $a = "ManejadorSQL";
+     $b = 'MySQL';
+     $c = &$a;
+
+     echo '<h4>A. Ahora muestra el contenido de cada variable</h4>';
+
+     echo $a;
+     echo '<br>';
+     echo $b;
+     echo'<br>';
+     echo $c;
+     
+     echo '<h4>B. Agrega al código actual las asignaciones correspondientes</h4>';
+     
+     $a = "PHP server";
+     $b = &$a;
+     echo '<br>';
+     echo '<h4>C. Vuelve a mostar el contenido de cada uno</h4>';
+
+     echo '<br>';
+     echo $a;
+     echo '<br>';
+     echo $b;
+     echo'<br>';
+     echo $c;
+
+     echo '<br>';
+     echo '<h4>D. Describe y muestra que ocurrio en el segundo bloque de asignaciones</h4>';
+     echo '<br>';
+     echo '<p>En el primer bloque de asignaciones tanto la variable "a" como "b" tiene como valor una cadena de texto y la 
+     variable "c" tiene el valor de la variable "a", por ello al mostrar el contenido de las variables, "a" y "c" muestran la 
+     misma cadena. En el segundo bloque de asignaciones se cambia la cadena de texto que tiene la variable "a" y a la variable "b"
+     se le da el valor de la variable "a"; cuando volvemos a mostrar el contenido de las variables, todas muestran la misma cadena
+     de texto, ya que a la variable "a" se le ha asignado de forma directa y tanto "b" como "c" tiene el valor de "a". </p>';
+
+
+     ?>
 
 </body>
 </html>
