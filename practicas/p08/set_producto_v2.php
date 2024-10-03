f69cfd7ff989074917e8129739ab80552025f075
Erwin <56erwinavendano@gmail.com>
2024-10-03 16:23:58 -0600

    Validacion de productos

--- practicas/p08/set_producto_v2.php
+++ practicas/p08/set_producto_v2.php
@@ -17,28 +17,52 @@ if ($link->connect_errno)
     /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
 }
 
-if($nombre!=  &&  $marca!=  && $modelo!= ){
+
+$Validar = "SELECT FROM productos WHERE nombre = ? AND marca = ? AND modelo = ?";
+$Asignar->t_dato("sss", $nombre, $marca, $modelo);
+$Asignar->execute();
+$Coincidencia = $Asignar->get_result();
+
+if ($Coincidencia == 0){
+
+    /** Crear una tabla que no devuelve un conjunto de resultados */
+$sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}')";
+if ( $link->query($sql) ) 
+{
+    echo "<h2>Resumen del producto insertado<h2>";
+    echo "<ul>";
+    echo "<li>Nombre: $nombre </li>";
+    echo "<li>Marca: $marca </li>";
+    echo "<li>Modelo: $modelo </li>";
+    echo "<li>Precio: $precio </li>";
+    echo "<li>Detalles: $Detalles</li>";
+    echo "<li>Numero de unidades: $Unidades</li>";
+    echo "<li>URL de la imagen: $Imagen</li>";
+    echo "</ul>";
+}
+else
+{
+	echo 'El Producto no pudo ser insertado =(';
+}
 
 
 }
 
 else {
 
-    echo "Los datos ingresados ya han sido registrados ";
+    echo "Error al validar los datos, el producto que intenta resgistrar ya se encuentra en la base de datos ";
 }
 
 
+/*
+$Validar = "SELECT FROM productos WHERE nombre = ? AND marca = ? AND modelo = ?"
+$Asignar2 = $link->prepare($Validar);
+*/
 
-/** Crear una tabla que no devuelve un conjunto de resultados */
-$sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}')";
-if ( $link->query($sql) ) 
-{
-    echo 'Producto insertado con ID: '.$link->insert_id;
-}
-else
-{
-	echo 'El Producto no pudo ser insertado =(';
-}
+
+
+
+
 
 $link->close();
 ?>
