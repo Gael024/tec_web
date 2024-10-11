52ac53563d76db3b7141a0acba4180f31949a2c9
Erwin <56erwinavendano@gmail.com>
2024-10-11 13:36:13 -0600

    Correcion

--- practicas/p09/get_productos_vigentes_v2.php
+++ practicas/p09/get_productos_vigentes_v2.php
@@ -81,7 +81,7 @@
             echo '<td>'.$row['unidades'].'</td>';
             echo '<td>'.utf8_encode($row['detalles']).'</td>';
             echo '<td><img src="'.$row['imagen'].'" ></td>';
-            echo '<td> <input type = "button" value="modificar" onclick = "Modificar();"> </td>';
+            echo '<td> <input type = "button" value="Modificar" onclick = "Modificar(event, ' . $row['id'] . ');"> </td>';
             echo '</tr>';
                     }
 
