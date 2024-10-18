0f36bc568e7e44933433a372a00ac52eac3817ce
Erwin <56erwinavendano@gmail.com>
2024-10-18 01:13:03 -0600

    Ejercicio 3

--- practicas/p10/product_app/app.js
+++ practicas/p10/product_app/app.js
@@ -180,3 +180,115 @@ function init() {
     var JsonString = JSON.stringify(baseJSON,null,2);
     document.getElementById("description").value = JsonString;
 }
+
+
+
+/*Validar Datos */
+
+
+function validar_formulario (){
+
+    var nombre = document.getElementById('nom').value;
+    var marca = document.getElementById('marca').value;
+    var modelo = document.getElementById('modelo').value;
+    var precio = document.getElementById('precio').value;
+    var detalles = document.getElementById('detalles').value;
+    var unidades = document.getElementById('unidades').value;
+    var imagen = document.getElementById('imagen').value;
+    
+    
+            if (nombre === "") {
+            
+                alert('El nombre del producto es obligatorio, por favor rellenar el campo');
+                event.preventDefault();
+                return;
+            }
+            
+            if(nombre.length > 100){
+            
+            alert('El nombre del producto no puede ser mayor a 100 caracteres');
+               event.preventDefault();
+               return;
+            
+            }
+    
+            
+                if(marca === "") {
+            
+                    alert('Por favor seleccione una marca');
+                    event.preventDefault();
+                    return;
+                }
+            
+            
+            
+                if(modelo === ""){
+            
+                    alert('El modelo del producto es obligatorio, por favor rellenar el campo');
+                    event.preventDefault();
+                    return;
+            
+                }
+            
+                if(modelo.length > 25) {
+            
+                    alert('El modelo del producto no puede superar los 25 caracteres');
+                    event.preventDefault();
+                    return;
+                }
+    
+                
+            
+                if(precio === "") {
+            
+                    alert('El producto no puede ingresarse sin un precio');
+                    event.preventDefault();
+                    return;
+            
+                }
+            
+                if(precio <100){
+            
+                    alert('El precio minimo de un producto es de $100');
+                    event.preventDefault();
+                    return;
+                }
+    
+                
+            
+                if(detalles > 250) {
+            
+                    alert('Los detalles del producto no deben superar los 250 caracteres');
+                    event.preventDefault();
+                    return;
+                }
+    
+                
+            
+                if(unidades === "") {
+            
+                    alert('El campo "unidades" no puede quedar vacio');
+                    event.preventDefault();
+                    return;
+                }
+            
+                if(unidades < 0){
+            
+                    alert('El valor minimo del campo "unidades" es 0');
+                    event.preventDefault();
+                    return;
+                }
+    
+                
+            
+                if (imagen === "") {
+            
+                    document.getElementById('imagen').value = "img/predeterminada.jpg";
+                    alert('No se ha ingresado ninguna imagen, el producto se mostrará con una imagen por defecto');
+                    return;
+                }
+    
+    
+    }
+    
+
--- practicas/p10/product_app/backend/create.php
+++ practicas/p10/product_app/backend/create.php
@@ -6,11 +6,89 @@
     if(!empty($producto)) {
         // SE TRANSFORMA EL STRING DEL JASON A OBJETO
         $jsonOBJ = json_decode($producto);
+
+
+
+
+
+
+        $nombre = $_POST['nombre'];
+        $marca  = $_POST['marca'];
+        $modelo = $_POST['modelo'];
+        $precio = $_POST['precio'];
+        $detalles = $_POST['detalles'];
+        $unidades = $_POST['unidades'];
+        $imagen = $_POST['imagen'];
+        $eliminado = $_POST['eliminado'];
+
+        @$link = new mysqli('localhost', 'root', '', 'marketzone');	
+
+        /** comprobar la conexión */
+if ($link->connect_errno) 
+{
+    die('Falló la conexión: '.$link->connect_error.'<br/>');
+    /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+}
+
+
+$Validar = "SELECT * FROM productos WHERE nombre = ? AND eliminado = 0 ";
+$Asignar = $link->prepare($Validar);
+$Asignar->bind_param("si", $nombre, $eliminado);
+$Asignar->execute();
+$Coincidencia = $Asignar->get_result();
+
+
+if ($Coincidencia->num_rows == 0){
+      
+$sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}',0)";
+        
+/*
+$sql = "INSERT INTO productos ( nombre, marca, modelo, precio, detalles, unidades, imagen) VALUES 
+    ('{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}')";
+*/
+
+
+if ( $link->query($sql) ) 
+{
+    echo "<h2>Resumen del producto insertado<h2>";
+    echo "<ul>";
+    echo "<li>Nombre: $nombre </li>";
+    echo "<li>Marca: $marca </li>";
+    echo "<li>Modelo: $modelo </li>";
+    echo "<li>Precio: $precio </li>";
+    echo "<li>Detalles: $detalles</li>";
+    echo "<li>Numero de unidades: $unidades</li>";
+    echo "<li>URL de la imagen: $imagen</li>";
+    echo "</ul>";
+}
+else
+{
+	echo 'El Producto no pudo ser insertado =(';
+}
+
+
+}
+
+
+else {
+
+    echo "<h3>Error al validar los datos, el producto que intenta resgistrar ya se encuentra en la base de datos</h3> ";
+}
+
+
+
         /**
          * SUSTITUYE LA SIGUIENTE LÍNEA POR EL CÓDIGO QUE REALICE
          * LA INSERCIÓN A LA BASE DE DATOS. COMO RESPUESTA REGRESA
          * UN MENSAJE DE ÉXITO O DE ERROR, SEGÚN SEA EL CASO.
          */
+
+
+
+
+
         echo '[SERVIDOR] Nombre: '.$jsonOBJ->nombre;
     }
+
+  /* */  $link->close();
 ?>
