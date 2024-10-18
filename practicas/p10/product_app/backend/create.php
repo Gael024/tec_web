d3fe6997612cc3b3e80338dbf056452eaa8b4dca
Erwin <56erwinavendano@gmail.com>
2024-10-18 06:04:27 -0600

    Ajuste

--- practicas/p10/product_app/backend/create.php
+++ practicas/p10/product_app/backend/create.php
@@ -55,18 +55,19 @@ $sql = "INSERT INTO productos ( nombre, marca, modelo, precio, detalles, unidade
 
 if ( $insertar->execute() ) 
 {
-   echo 'Producto insertado correctamente';
+    echo json_encode(['status' => 'success', 'message' => 'El producto se ha insertado correctamente']);
 }
 else
 {
-	echo 'El Producto no pudo ser insertado =(';
+    echo json_encode(['status' => 'error', 'message' => 'Ha ocurrido un error, el producto no se pudo insertar']);
 }
 $insertar->close();
 }
 
 else {
 
-    echo "<h3>Error al validar los datos, el producto que intenta resgistrar ya se encuentra en la base de datos</h3> ";
+    echo json_encode(['status' => 'error', 'message' => 'El producto que está intentando ingresar ya se encuentra en la base de datos']);
+}
 }
 
 
