4af87ff3e868cc51d02c693a668403ba2ba46519
Erwin <56erwinavendano@gmail.com>
2024-10-23 14:27:38 -0600

    Correccion 4 ejercicio 3

--- practicas/p11/p11_con_jquery/product_app/backend/product-list.php
+++ practicas/p11/p11_con_jquery/product_app/backend/product-list.php
@@ -1,28 +1,24 @@
 <?php
-    include_once __DIR__.'/database.php';
+   include('database.php');
 
-    // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
-    $data = array();
+   $query = "SELECT * from productos WHERE eliminado = 0";
+   $result = mysqli_query($conexion, $query);
 
-    // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
-    if ( $result = $conexion->query("SELECT * FROM productos WHERE eliminado = 0") ) {
-        // SE OBTIENEN LOS RESULTADOS
-        $rows = $result->fetch_all(MYSQLI_ASSOC);
-
-        if(!is_null($rows)) {
-            // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
-            foreach($rows as $num => $row) {
-                foreach($row as $key => $value) {
-                    $data[$num][$key] = utf8_encode($value);
-                }
-            }
-        }
-        $result->free();
-    } else {
-        die('Query Error: '.mysqli_error($conexion));
-    }
-    $conexion->close();
-    
-    // SE HACE LA CONVERSIÓN DE ARRAY A JSON
-    echo json_encode($data, JSON_PRETTY_PRINT);
+   if(!$result){
+       die('Query Failed'.mysqli_error($conexion));
+   }
+   $json = array();
+   while($row = mysqli_fetch_array($result)){
+       $json[] = array(
+           'id'=> $row['id'],
+           'nombre'=> $row['nombre'],
+           'precio'=> $row['precio'],
+           'unidades'=> $row['unidades'],
+           'marca'=> $row['marca'],
+           'modelo'=> $row['modelo'],
+           'detalles'=> $row['detalles']
+       );
+   }
+   $jsonstring = json_encode($json);
+   echo $jsonstring; 
 ?>
