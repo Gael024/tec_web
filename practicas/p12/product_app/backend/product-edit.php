997d4475fc32b29201374c7f7d3c7fc26a1518d4
Erwin <56erwinavendano@gmail.com>
2024-11-11 21:57:17 -0600

    Practica 12 Final

--- practicas/p12/product_app/backend/product-edit.php
+++ practicas/p12/product_app/backend/product-edit.php
@@ -1,8 +1,8 @@
 <?php
-    use TECWEB\MYAPI\Products;
+    use TECWEB\MYAPI\Update;
     require_once __DIR__.'/myapi/Products.php';
 
-    $productos = new Products('marketzone');
+    $productos = new Update('marketzone');
     $productos->edit( json_decode( json_encode($_POST) ) );
     echo $productos->getData();
 ?>
